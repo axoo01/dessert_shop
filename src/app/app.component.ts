dfc884f1eabadb6dde3276950466dc867cafd481
axoo01 <juniorax03@gmail.com>
2026-04-25 16:12:11 +0200

    style: implement responsive grid layout and typography for main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 @Component({
+
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProductListComponent, ShoppingCartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
