3c993fec5491e961d098a804a380d816a3ae3f0d
axoo01 <juniorax03@gmail.com>
2026-04-25 14:36:55 +0200

    feat: define product interface and generate core components

---
+++ src/app/models/product.interface.ts
@@ -0,0 +1,13 @@
+export interface ProductImage {
+  thumbnail: string;
+  mobile: string;
+  tablet: string;
+  desktop: string;
+}
+
+export interface Product {
+  image: ProductImage;
+  name: string;
+  category: string;
+  price: number;
+}
