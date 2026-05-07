131ceb0ff8e1f8b64eff524a8a1574dda3121edf
axoo01 <juniorax03@gmail.com>
2026-05-07 11:05:01 +0200

    feat: added highlights for product feedback

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, inject, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../models/product.interface'; 
 import { CartService } from '../../services/cart.service';
+// 1. Import the service
+import { ProductHighlightService } from '../../services/product-highlight.service';
 
 @Component({
   selector: 'app-product-item',
@@ -11,23 +13,36 @@ import { CartService } from '../../services/cart.service';
   styleUrl: './product-item.component.scss'
 })
 export class ProductItemComponent {
-  
   @Input({ required: true }) product!: Product; 
 
   private cartService = inject(CartService);
+
+  private highlightService = inject(ProductHighlightService);
+
+  isHighlighted = computed(() => 
+    this.highlightService.highlightedProductName() === this.product.name
+  );
 
   cartItem = computed(() => 
     this.cartService.items().find(i => i.name === this.product.name)
   );
 
+  
+  onMouseEnter() {
+    this.highlightService.setHighlight(this.product.name);
+  }
+
+  onMouseLeave() {
+    this.highlightService.setHighlight(null);
+  }
+
   onAddToCart() {
     this.cartService.addToCart(this.product);
   }
 
- 
-onDecrement() {
-  if (this.cartItem()) {
-    this.cartService.updateQuantity(this.product.name, -1);
+  onDecrement() {
+    if (this.cartItem()) {
+      this.cartService.updateQuantity(this.product.name, -1);
+    }
   }
-}
 }
